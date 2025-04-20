7b7859a68042d3030453d70c5ee3d32ce500d245
Alexandre Guenot <alexandre.guenot@gmail.com>
2025-04-20 21:57:13 +0200

    feat(codium): add codium config files

---
+++ codium/dot_config.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+app_name='codium'
+config_path="${HOME}/.config/VSCodium/User"
+check_command='codium --version'
+main_config_subpath='user-config/'
+
+additional_setup() {
+    set -e
+
+    log_info "Adding extensions"
+    local extensions_dir="${HOME}/.vscode-oss/extensions"
+    if [ -d "$extensions_dir" ]; then
+        log_warning "Found existing VSCodium extensions, backing up to ${extensions_dir}.bak"
+        mv "$extensions_dir" "${extensions_dir}.bak"
+    fi
+
+    mkdir -p "${HOME}/.vscode-oss"
+    ln -s "${REPO_PATH}/codium/extensions/" "$extensions_dir"
+
+    log_info "Extensions installed"
+
+    set +e
+}
